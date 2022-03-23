d79b0590d2f34157c1b9adeaf51d0e5f71a0fe74
mukeshshinde <mukeshshinde1995@mail.com>
2022-03-24 00:12:02 +0530

    project add

---
+++ WebService1/src/main/java/com/cjc/main/WebService1Application.java
@@ -0,0 +1,14 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebService1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Web Service 1");
+		SpringApplication.run(WebService1Application.class, args);
+	}
+
+}
